713d522f585367385dc153ff5048f4d26ffde09f
Reljod Oreta <oretareljod@gmail.com>
2024-08-11 19:41:41 +0800

    Add harpoon2 lavyvim extra plugin

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Set leader key to space
+local keymap = vim.keymap
+
+-- General keymaps
+keymap.set("n", "<leader>wq", ":wq<CR>", { desc = "Save and quit current file" }) -- save and quit
+keymap.set("n", "<leader>qq", ":q!<CR>", { desc = "Force quit file" }) -- quit without saving
+keymap.set("n", "<leader>ww", ":w<CR>", { desc = "Save current file" }) -- save
