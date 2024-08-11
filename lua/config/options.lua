63c6eb3c9f10bf0ee7940a5a8c83a20469617407
Reljod Oreta <oretareljod@gmail.com>
2024-08-12 01:07:16 +0800

    Add custom options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,3 +5,19 @@
 -- Enable the option to require a Prettier config file
 -- If no prettier config file is found, the formatter will not be used
 vim.g.lazyvim_prettier_needs_config = false
+
+-- Line Numbers
+vim.opt.relativenumber = true
+vim.opt.number = true
+vim.opt.nu = true
+vim.o.statuscolumn = "%s %l %r "
+
+-- Set highlight on search, but clear on pressing <Esc> in normal mode
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+
+-- Scrolling
+vim.opt.scrolloff = 10
+
+-- Remove LazyVim Autoformat
+vim.g.autoformat = false
