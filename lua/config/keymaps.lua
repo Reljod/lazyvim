9e3910ab67649b1e1dc88dcf43f678c0268f65aa
Reljod Oreta <oretareljod@gmail.com>
2024-08-11 19:42:56 +0800

    Add custom general keymaps from my previous nvim plugin

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,5 +7,18 @@ local keymap = vim.keymap
 
 -- General keymaps
 keymap.set("n", "<leader>wq", ":wq<CR>", { desc = "Save and quit current file" }) -- save and quit
-keymap.set("n", "<leader>qq", ":q!<CR>", { desc = "Force quit file" }) -- quit without saving
+keymap.set("n", "<leader>qq", ":q<CR>", { desc = "Quit file" }) -- quit
+keymap.set("n", "<leader>qQ", ":q!<CR>", { desc = "Force Quit file" }) -- quit without saving
 keymap.set("n", "<leader>ww", ":w<CR>", { desc = "Save current file" }) -- save
+-- Center screen while doing motions
+keymap.set("n", "<C-d>", "<C-d>zz")
+keymap.set("n", "<C-u>", "<C-u>zz")
+keymap.set("n", "n", "nzzzv")
+keymap.set("n", "N", "Nzzzv")
+-- Search and Replace current word in the file
+keymap.set(
+  "n",
+  "<leader>sR",
+  [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+  { desc = "Search and Replace Current Word in File" }
+)
